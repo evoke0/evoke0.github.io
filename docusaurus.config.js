d0586189ec5fcddf0ef0ff84d5b401f4937e843e
evoke0 <ale-jimenez1@hotmail.es>
2024-10-08 20:36:33 +0200

    Fix deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://kalpa-desktop-web-test.github.io',
+  url: 'https://kalpa-web.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'evoke0', // Usually your GitHub org/user name.
-  projectName: 'kalpa-desktop-web-test.github.io', // Usually your repo name.
+  projectName: 'kalpa-web.github.io', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: 'main',
 
